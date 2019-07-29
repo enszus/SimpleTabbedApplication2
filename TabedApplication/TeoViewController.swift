8bd64178e7ccc32d76bb56f40d38e6088f8d30d0
Teodor Ploae <ploaetdr@yahoo.com>
2019-07-29 16:39:11 +0300

    Added TeoViewController

---
+++ TabedApplication/TeoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TeoViewController.swift
+//  TabedApplication
+//
+//  Created by Teodor Ploae on 29/07/2019.
+//  Copyright © 2019 Endava. All rights reserved.
+//
+
+import UIKit
+
+class TeoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
