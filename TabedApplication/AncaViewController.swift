0935eda2c1b2fc1e0326332ef6da0b89a1c7ae31
Anca Grigoriu <grigoriu.anca2012@gmail.com>
2019-07-29 15:38:14 +0300

    Added ViewController

---
+++ TabedApplication/AncaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AncaViewController.swift
+//  TabedApplication
+//
+//  Created by Anca Grigoriu on 29/07/2019.
+//  Copyright © 2019 Endava. All rights reserved.
+//
+
+import UIKit
+
+class AncaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
